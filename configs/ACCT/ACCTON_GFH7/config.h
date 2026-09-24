a6917d2be0173f8abf49ac4c38525913c451add0
RunnyCow <thankyous@gmail.com>
2026-09-24 12:18:55 +0800

    1. Select LSM6DSK320X for the IMU preset. 2. Modify LEDx_PIN defined 3. Support MAG and OSD

--- configs/ACCT/ACCTON_GFH7/config.h
+++ configs/ACCT/ACCTON_GFH7/config.h
@@ -25,14 +25,15 @@
 
 #define BOARD_NAME        ACCTON_GFH7
 #define MANUFACTURER_ID   ACCT
-
 #define USE_ACC
-#define USE_ACC_SPI_ICM42688P
+#define USE_ACCGYRO_LSM6DSK320X
 #define USE_GYRO
-#define USE_GYRO_SPI_ICM42688P
 #define USE_BARO
 #define USE_BARO_DPS310
 #define USE_SDCARD
+#define USE_SDCARD_SDIO
+#define USE_MAG
+#define USE_MAX7456
 #define USE_GYRO_CLKIN
 
 #define BEEPER_PIN PE12 // BUZZER
@@ -64,7 +65,6 @@
 #define I2C1_SDA_PIN PB7
 #define I2C4_SCL_PIN PD12
 #define I2C4_SDA_PIN PD13
-#define LED0_PIN     PE10
 #define SPI1_SCK_PIN PA5
 #define SPI2_SCK_PIN PD3
 #define SPI4_SCK_PIN PE2
@@ -77,10 +77,12 @@
 #define ADC_VBAT_PIN PC0
 #define ADC_CURR_PIN PB0
 #define ADC_RSSI_PIN PB1
-#define GYRO_1_EXTI_PIN PD10
-#define GYRO_1_CS_PIN PE4
+#define LED0_PIN     PC13
+#define LED1_PIN     PE10
+#define GYRO_1_EXTI_PIN PD11
+#define GYRO_1_CS_PIN PB9
 #define GYRO_1_ALIGN CW0_DEG
-#define GYRO_1_CLKIN_PIN PD14
+#define MAX7456_SPI_CS_PIN    PA4
 #define SDIO_CK_PIN   PD6
 #define SDIO_CMD_PIN  PA0
 #define SDIO_D0_PIN   PB14
@@ -88,6 +90,7 @@
 #define SDIO_D2_PIN   PB3
 #define SDIO_D3_PIN   PB4
 #define SDCARD_DETECT_PIN PA15
+#define SDCARD_DETECT_INVERTED
 
 #define BOX_USER1_NAME      "VTX SWITCH"
 #define PINIO1_BOX    40
@@ -97,19 +100,21 @@
 #define PINIO2_PIN    PE3 // heater switch pin
 
 #define TIMER_PIN_MAPPING \
-   TIMER_PIN_MAP( 0, MOTOR1_PIN,       1,  0) \ /* TIM1_CH1, DMA Opt 0 */ \
-   TIMER_PIN_MAP( 1, MOTOR2_PIN,       1,  1) \ /* TIM1_CH2, DMA Opt 1 */ \
-   TIMER_PIN_MAP( 2, MOTOR3_PIN,       1,  2) \ /* TIM1_CH3, DMA Opt 2 */ \
-   TIMER_PIN_MAP( 3, MOTOR4_PIN,       1,  3) \ /* TIM1_CH4, DMA Opt 3 */ \
-   TIMER_PIN_MAP( 4, LED_STRIP_PIN,    1,  4) \ /* TIM2_CH3 */ \
-   TIMER_PIN_MAP( 5, GYRO_1_CLKIN_PIN, 1, -1) \ /* TIM4_CH3, No DMA */ \
-   TIMER_PIN_MAP( 6, SERVO1_PIN,       1, -1) \ /* TIM3_CH1, No DMA */ \
-   TIMER_PIN_MAP( 7, SERVO2_PIN,       1, -1) \ /* TIM3_CH2, No DMA */ \
-   TIMER_PIN_MAP( 8, SERVO3_PIN,       1, -1) \ /* TIM3_CH3, No DMA */ \
-   TIMER_PIN_MAP( 9, SERVO4_PIN,       1, -1)   /* TIM3_CH4, No DMA */
+   TIMER_PIN_MAP( 0, MOTOR1_PIN,       1,  0) /* TIM1_CH1, DMA Opt 0 */ \
+   TIMER_PIN_MAP( 1, MOTOR2_PIN,       1,  1) /* TIM1_CH2, DMA Opt 1 */ \
+   TIMER_PIN_MAP( 2, MOTOR3_PIN,       1,  2) /* TIM1_CH3, DMA Opt 2 */ \
+   TIMER_PIN_MAP( 3, MOTOR4_PIN,       1,  3) /* TIM1_CH4, DMA Opt 3 */ \
+   TIMER_PIN_MAP( 4, LED_STRIP_PIN,    1,  4) /* TIM2_CH3 */ \
+   TIMER_PIN_MAP( 5, SERVO1_PIN,       1, -1) /* TIM3_CH1, No DMA */ \
+   TIMER_PIN_MAP( 6, SERVO2_PIN,       1, -1) /* TIM3_CH2, No DMA */ \
+   TIMER_PIN_MAP( 7, SERVO3_PIN,       1, -1) /* TIM3_CH3, No DMA */ \
+   TIMER_PIN_MAP( 8, SERVO4_PIN,       1, -1) /* TIM3_CH4, No DMA */
 
 #define ADC_INSTANCE  ADC1
-#define ADC1_DMA_OPT 10
+#define ADC1_DMA_OPT                    9
+#define ADC3_DMA_OPT                    10
+#define TIMUP8_DMA_OPT                  11
+#define TIMUP1_DMA_OPT                  12
 
 #define BARO_I2C_INSTANCE I2CDEV_1
 #define MAG_I2C_INSTANCE I2CDEV_4
@@ -118,12 +123,13 @@
 #define DEFAULT_DSHOT_BITBANG  DSHOT_BITBANG_ON
 #define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
 #define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
-#define DEFAULT_VOLTAGE_METER_SCALE      170
+#define DEFAULT_VOLTAGE_METER_SCALE      136
 
 #define LED0_INVERTED
 #define LED1_INVERTED
 #define BEEPER_INVERTED
 #define SYSTEM_HSE_MHZ 16
-#define GYRO_1_SPI_INSTANCE SPI4
+#define GYRO_1_SPI_INSTANCE SPI2
 #define SDIO_DEVICE SDIODEV_2
 #define SDIO_USE_4BIT 1
+#define MAX7456_SPI_INSTANCE SPI1
